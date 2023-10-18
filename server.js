3e0088e53424e96df9b51b649655a4916108918a
Abhinav Kumar <ady.abhinav.ak@gmail.com>
2023-10-18 16:34:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const corsOptions = {
-    origin: 'https://betaapi-9jpy.onrender.com', // Allow requests from http://localhost:3001  };
+    origin: '*', // Allow requests from http://localhost:3001  };
 }
 app.use(cors(corsOptions));
 
